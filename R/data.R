af8683b2fbcd6c156c123d7cf2bae687c96d3781
Baodong Liu <bblpo@yahoo.com>
2018-02-16 17:59:56 -0700

    commit11

---
+++ R/data.R
@@ -0,0 +1,22 @@
+
+#' virtual. dataset for replication study of Kittilson and Dalton (2011)
+#' Original article used virtual. Kittilson, Miki Caul and Russell J. Dalton. (2011). “Virtual Civil Society: The New Frontier of Social Capital?” Political Behavior: Volume 33, Issue 4, pp 625-644
+#'
+#' virtual. The newly added variables are as follows:
+#' \itemize{
+#'   \item tolerance_scale. the response variable for model 1 of Table 4, Kittilson and Dalton (2011)
+#'   \item duty_based_index. the response variable for model 2 of Table 4, Kittilson and Dalton (2011)
+#'   \item engagement_index. the response variable for model 3 of Table 4, Kittilson and Dalton (2011)
+#'   \item Factor1. the response variable for model 5 of Table 4, Kittilson and Dalton (2011)
+#'   \item Factor2. the response variable for model 6 of Table 4, Kittilson and Dalton (2011)
+#'   \item Factor3. the response variable for model 7 of Table 4, Kittilson and Dalton (2011)
+#'   \item Factor4. the response variable for model 1 of Table 4, Kittilson and Dalton (2011)
+#' }
+#'
+#' @format A data frame with 1001 rows and 804 variables
+#' @source \url{https://www.icpsr.umich.edu/icpsrweb/ICPSR/studies/4607}
+#' @docType data
+#' @keywords datasets
+#' @name virtual
+#' @usage data(virtual)
+NULL
--- R/diagnosis-function.R
+++ R/diagnosis-function.R
@@ -3,23 +3,6 @@
 #' @param  x an object, which is a result from a linear model (lm)
 #' @param  file is the name of text file you want to create (e.g.,'mydiagnosis.txt'). It is iniated as 'result.txt'
 #' Note that the file will be saved at the working directory
-#' virtual. dataset for replication study of Kittilson and Dalton (2011)
-#' Original article used virtual. Kittilson, Miki Caul and Russell J. Dalton. (2011). “Virtual Civil Society: The New Frontier of Social Capital?” Political Behavior: Volume 33, Issue 4, pp 625-644
-#'
-#' virtual. The newly added variables are as follows:
-#' \itemize{
-#'   \item tolerance_scale. the response variable for model 1 of Table 4, Kittilson and Dalton (2011)
-#'   \item duty_based_index. the response variable for model 2 of Table 4, Kittilson and Dalton (2011)
-#'   \item engagement_index. the response variable for model 3 of Table 4, Kittilson and Dalton (2011)
-#'   \item Factor1. the response variable for model 5 of Table 4, Kittilson and Dalton (2011)
-#'   \item Factor2. the response variable for model 6 of Table 4, Kittilson and Dalton (2011)
-#'   \item Factor3. the response variable for model 7 of Table 4, Kittilson and Dalton (2011)
-#'   \item Factor4. the response variable for model 1 of Table 4, Kittilson and Dalton (2011)
-#' }
-#'
-#' @format A data frame with 1001 rows and 804 variables
-#' @source \url{https://www.icpsr.umich.edu/icpsrweb/ICPSR/studies/4607}
-
 #' @export
 #' @return The statistical tests results, outlierTest, durbinWatsonTest, VIF, and gvlma
 
